95ce7700bfdf4d5b80954d221386c1247dcdb418
Valdo Romao <v.romao@thecodingmachine.com>
2022-02-04 10:18:07 +0000

    Update and fixed silent zones

--- src/index.ts
+++ src/index.ts
@@ -21,3 +21,63 @@ WA.onInit().then(() => {
         console.log('Scripting API Extra ready');
     }).catch(e => console.error(e));
 }).catch(e => console.error(e));
+
+let currentZone: string;
+let currentPopup: any;
+
+const config = [
+    {
+        zone: 'needHelp',
+        message: 'Do you need some guidance? We are happy to help you out.',
+        cta: [
+            {
+                label: 'Meet us',
+                className: 'primary',
+                callback: () => WA.nav.openTab('https://play.staging.workadventu.re/@/tcm/workadventure/wa-village'),
+            }
+        ]
+    },
+    {
+        zone: 'followUs',
+        message: 'Hey! Have you already started following us?',
+        cta: [
+            {
+                label: 'LinkedIn',
+                className: 'primary',
+                callback: () => WA.nav.openTab('https://www.linkedin.com/company/workadventu-re'),
+            },
+            {
+                label: 'Twitter',
+                className: 'primary',
+                callback: () => WA.nav.openTab('https://twitter.com/workadventure_'),
+            }
+        ]
+    },
+]
+
+WA.onEnterZone('needHelp', () => {
+    currentZone = 'needHelp'
+    openPopup(currentZone, currentZone + 'Popup')
+});
+WA.onEnterZone('followUs', () => {
+    currentZone = 'followUs'
+    openPopup(currentZone, currentZone + 'Popup')
+});
+WA.onLeaveZone('needHelp', closePopup);
+WA.onLeaveZone('followUs', closePopup);
+
+function openPopup(zoneName: string, popupName: string) {
+    const zone = config.find((item) => {
+        return item.zone == zoneName
+    });
+    if (typeof zone !== 'undefined') {
+        // @ts-ignore otherwise we can't use zone.cta object
+        currentPopup = WA.openPopup(popupName, zone.message, zone.cta)
+    }
+}
+function closePopup(){
+    if (typeof currentPopup !== undefined) {
+        currentPopup.close();
+        currentPopup = undefined;
+    }
+}
